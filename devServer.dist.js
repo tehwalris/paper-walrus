8580aadb79d11faebceac5ff0d6adabd75b10b56
Philippe Voinov <philippevoinov@gmail.com>
2016-06-23 20:32:17 +0200

    History api fallback in devServer dist.

--- devServer.dist.js
+++ devServer.dist.js
@@ -1,11 +1,10 @@
 const express = require('express'),
   httpProxy = require('http-proxy'),
+  fallback = require('express-history-api-fallback'),
   config = require('./devServer.config');
 
 const app = express();
 const apiProxy = httpProxy.createProxyServer();
-
-app.use('/', express.static('./dist'));
 
 app.use('/api', function(req, res) {
   apiProxy.web(req, res, {
@@ -14,6 +13,10 @@ app.use('/api', function(req, res) {
     pathRewrite: {'^/api': '/'},
   });
 });
+
+const webroot = __dirname + '/dist';
+app.use('/', express.static(webroot));
+app.use(fallback('index.html', {root: webroot}));
 
 app.listen(config.port, config.host, () => {
   console.log(`Listening on ${config.host} port ${config.port}.`);
