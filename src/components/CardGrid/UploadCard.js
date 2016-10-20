e7897cdbbf9c235f3790848f614708f59e096ecf
Philippe Voinov <philippevoinov@gmail.com>
2016-10-20 20:29:45 +0200

    Removed unused code.

--- src/components/CardGrid/UploadCard.js
+++ src/components/CardGrid/UploadCard.js
@@ -55,5 +55,3 @@ export default class UploadCard extends Component {
     };
   }
 }
-
-
--- src/util/resolveSources.js
+++
@@ -1,8 +0,0 @@
-import {contentBaseUrl} from '../util/apiConfig';
-
-export default function resolveSources (data) {
-  return {
-    original: contentBaseUrl + '/' + data.originalFile,
-    preview: data.previewFile ? contentBaseUrl + '/' + data.previewFile : null,
-  };
-}
