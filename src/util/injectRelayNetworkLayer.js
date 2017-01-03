857e4f760bd596a4427ab296513015e9f625972c
Philippe Voinov <philippevoinov@gmail.com>
2017-01-03 14:32:28 +0100

    Disable batch queries for now.

--- src/util/injectRelayNetworkLayer.js
+++ src/util/injectRelayNetworkLayer.js
@@ -3,13 +3,18 @@ import {RelayNetworkLayer, urlMiddleware} from 'react-relay-network-layer';
 import authMiddleware from './authMiddleware'
 import {store} from '../common';
 
-Relay.injectNetworkLayer(new RelayNetworkLayer([
-  urlMiddleware({
-    url: () => '/graphql',
-  }),
-  authMiddleware({
-    token: () => store.getState().user.token,
-    prefix: '',
-    header: 'x-access-token',
-  }),
-]));
+Relay.injectNetworkLayer(new RelayNetworkLayer(
+  [
+    urlMiddleware({
+      url: () => '/graphql',
+    }),
+    authMiddleware({
+      token: () => store.getState().user.token,
+      prefix: '',
+      header: 'x-access-token',
+    }),
+  ],
+  {
+    disableBatchQuery: true,
+  },
+));
